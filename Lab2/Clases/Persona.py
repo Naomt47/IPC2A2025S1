0a567c43f74e7ec683e0f2a14a398874a967c8ca
Naomi <3603870200101@ingenieria.usac.edu.gt>
2025-02-07 23:49:30 -0600

    Ejemplos Lab 1 y 2

---
+++ Lab2/Clases/Persona.py
@@ -0,0 +1,12 @@
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def saludar(self):
+        return f"Hola, yo soy {self.nombre}, tengo {self.edad} años"
+    
+
+    def despedirse(self):
+        return "Adiós"
+    
